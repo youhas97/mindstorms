d5020c45a48b81c12a2caf88fc073995a8ea527e
Hellux <hellux07@gmail.com>
2016-10-11 09:43:03 +0200

    added file for main program

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+
+from unit import Unit
+
+
+def main():
+    unit = Unit()
+
+    while True:
+        mode = patrol(unit)
+
+
+def patrol(unit):
+    return patrol
+
+
+def follow_tape(unit):
+    return follow_tape
+
+
+if __name__ == '__main__':
+    main()
