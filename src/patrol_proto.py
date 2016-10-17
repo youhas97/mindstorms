99e2d41148e078e53d0ac0c3ef6a4289c0a1694f
Yohan Ayoub <yohay608@student.liu.se>
2016-10-17 17:05:48 +0200

    milestone3

--- src/patrol_proto.py
+++ src/patrol_proto.py
@@ -1,19 +1,29 @@
 from unit import Unit
+from time import sleep
+from random import randint, choice
+
+channel = 1
+error_margin = 10
+velocity = 25
+distance = 40
 
 def patrol():
     print('hej')
     unit = Unit('192.168.0.111')
-
-    if unit.ir_sensor.get_prox() >= 50:
-        
-        unit.forward(10)
-        
-    else:
-        unit.stop
-        Ev3.speak('u what mate')
-        rotate.random.randint()
-        unit.forward(20)
-
+    
+    while True:
+	    direction = choice([-1,1])
+	    print(direction)
+	    prox = unit.ir_sensor.get_prox()
+	    print(prox)
+	    if prox >= distance:
+		    unit.forward(100)
+	    elif prox <= distance+error_margin:
+		    unit.stop()
+		    unit.speak('u what mate')
+		    sleep(1)
+		    unit.rotate(100,direction*randint(90,180))
+		    sleep(2)
 patrol()
         
             
