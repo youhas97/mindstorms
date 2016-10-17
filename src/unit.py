047c614c099e5fa5f69843c8ce5b6b6c7250401f
Hellux <hellux07@gmail.com>
2016-10-17 19:28:33 +0200

    removed alignment of declarations

--- src/unit.py
+++ src/unit.py
@@ -5,11 +5,11 @@ class Unit(Ev3):
         super().__init__(ip)
 
         # ports
-        left_motor =    'D'
-        right_motor =   'A'
-        gun =           'B'
-        ir_sensor =     2
-        color_sensor =  3
+        left_motor = 'D'
+        right_motor = 'A'
+        gun = 'B'
+        ir_sensor = 2
+        color_sensor = 3
 
         self.wheels = [left_motor, right_motor]
         self.left = self.add_motor(self.wheels[0])
