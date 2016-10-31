535721d8611522e4562b68145939ed284b175dc9
Yohan Ayoub <yohay608@student.liu.se>
2016-10-31 17:06:38 +0100

    if encounters intruder it counts down then attacks

--- src/threat.py
+++ src/threat.py
@@ -4,8 +4,7 @@ from time import sleep
 channel = 1
 error_margin = 10
 velocity = 25
-distance = 55
-
+distance = 50
 def threat():
     unit = Unit('192.168.0.111')
 	
@@ -13,16 +12,25 @@ def threat():
         """checks if encountered object is an intruder and attacks"""
         prox = unit.ir_sensor.get_prox()
         print(prox)
-        if prox <= distance+error_margin:
-            if 
-            if prox <= distance+error_margin:
-                unit.rotate(100,135)
-                sleep(0.8)
-                unit.speak('Say hello to my little friend')
-                sleep(2.6)
-                unit.start_gun(100)
-                sleep(4)
-                unit.stop_gun()
+        if prox <= distance:
+            if unit.check_movement(2,5) and prox <= distance:
+                unit.speak('get out')
+                sleep(1)
+                for seconds in ['five','four','three','two','one']:
+                    if unit.prox()<= distance:
+                        unit.speak(seconds)
+                    else:
+                        break 
+                    sleep(1)    
+                if seconds == 'one':
+                    unit.rotate(100,140)
+                    sleep(0.8)
+                    unit.speak('Say hello to my little friend')
+                    sleep(2.6)
+                    unit.start_gun(100)
+                    sleep(4)
+                    unit.stop_gun()
+                       
 threat()
 		   
 		
