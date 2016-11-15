ea314194b7024bc39d446fb6d0fa930d56a86855
Hellux <hellux07@gmail.com>
2016-11-15 18:44:11 +0100

    modified logger

--- src/thread.py
+++ src/thread.py
@@ -2,24 +2,29 @@ import logging
 from threading import Thread
 
 from unit import Unit
-import patrol
+
+import idle
 
 class GuardDog(Thread):
 
     def __init__(self):
         Thread.__init__(self)
-
         self.unit = None
+        self.log = logging.getLogger('dog')
+        self.log.setLevel(logging.INFO)
 
     def run(self):
-        self.mode = patrol.Patrol()
+        self.mode = idle.IdleMode(self.unit)
         while True:
             self.mode = self.mode.run(self.unit)
 
     def connect(self, address):
         try:
             self.unit = Unit(address)
+            self.start()
+            self.log.info('successfully connected -- \'{}\''.format(address))
+            return True
         except OSError as err:
-            logging.error('GuardDog: failed to connect -- {}'.format(address))
+            self.log.error('failed to connect -- \'{}\''.format(address))
+            self.log.error(err)
             return False
-        return True
