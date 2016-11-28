8440f44aa360bc84aa3c12a3f95775ab574cdb28
Yousef Hashem <youhas97@gmail.com>
2016-11-28 10:15:31 +0100

    Fixed some minor bugs

--- src/threat.py
+++ src/threat.py
@@ -8,7 +8,7 @@ class ThreatMode():
 
     def __init__(self):
         self.distance = 50
-        
+
     def detect_threat(self, unit):
         if unit.prox() <= self.distance:
             unit.stop()
@@ -18,27 +18,29 @@ class ThreatMode():
                 for seconds in ['five','four','three','two','one']:
                     if unit.prox() <= self.distance:
                         unit.speak(seconds)
-                        sleep(1)    
+                        sleep(1)
                     else:
                         unit.stop()
-                return seconds == 'one'
-            
+                        return False
+                return True
+
     def shoot(self, unit):
         unit.rotate(100,150)
         sleep(0.8)
         unit.speak('Say hello to my little friend')
-        sleep(2.6)
-        unit.shoot(3)
-        unit.rotate(100,150)
+        sleep(3.3)
+        unit.shoot(1)
+        sleep(1)
+        unit.rotate(100,180)
         sleep(2)
-        
+
     def run(self, unit):
         prox = unit.ir_sensor.get_prox()
         print(prox)
         if self.detect_threat(unit):
             self.shoot(unit)
             if unit.prox() <= self.distance:
-                return surrender.Surrender()
+                return surrender.Surrender(unit)
         else:
             return patrol.Patrol()
         return self
@@ -53,4 +55,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-		
+
