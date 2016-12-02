209ca50b08ab140ba211f0881774470c09292442
Hellux <hellux07@gmail.com>
2016-12-02 20:25:33 +0100

    tidying up

--- src/thread.py
+++ src/thread.py
@@ -18,14 +18,8 @@ class GuardDog(Thread):
 
     def __init__(self):
         Thread.__init__(self)
-
-        self.log = logging.getLogger('dog')
-        self.log.setLevel(logging.INFO)
-        console = logging.StreamHandler()
-        console.setLevel(logging.NOTSET)
-        formatter = logging.Formatter('%(levelname)s - %(name)s: %(message)s')
-        console.setFormatter(formatter)
-        self.log.addHandler(console)
+        
+        self.initiate_logger()
 
         self.unit = None
         self.NewMode = None
@@ -36,33 +30,47 @@ class GuardDog(Thread):
         self.actual_speed_str = tk.StringVar()
         self.distance_str = tk.StringVar()
 
+    def initiate_logger(self):
+        self.log = logging.getLogger('dog')
+        self.log.setLevel(logging.INFO)
+        console = logging.StreamHandler()
+        console.setLevel(logging.NOTSET)
+        formatter = logging.Formatter('%(levelname)s - %(name)s: %(message)s')
+        console.setFormatter(formatter)
+        self.log.addHandler(console)
+
     def set_mode(self, Mode):
         """Set mode."""
         self.NewMode = Mode
 
+    def execute_queue(self):
+        pass
+
+    def calculate_speed_distance(self):
+        self.time_prev = self.time
+        sellf.time = time()
+        time_delta = self.time - self.time_prev
+
+        self.actual_speed = (self.unit.actual_speed())
+        self.distance = (self.distance+self.actual_speed*time_delta)
+
+        self.actual_speed_str.set('{} m/s'.format(round(self.actual_speed, 2)))
+        self.distance_str.set('{} m'.format(round(self.distance, 2)))
+    
     def run(self):
         """Run an iteration of the unit's current mode."""
         self.mode = idle.IdleMode(self.unit)
-        time_prev = time_now = time()
+        self.time_prev = self.time_now = time()
 
         while True:
-            for func in self.queue:
-                func()
-            time_prev = time_now
-            time_now = time()
-            time_delta = time_now - time_prev
+            self.execute_queue()
+            self.calculate_speed_distance()
 
             self.mode = self.mode.run(self.unit)
             if self.NewMode:
                 self.mode = self.NewMode(self.unit)
                 self.NewMode = None
 
-            self.actual_speed = (self.unit.actual_speed())
-            self.distance = (self.distance+self.actual_speed*time_delta)
-
-            self.actual_speed_str.set('{} m/s'.format(round(self.actual_speed, 2)))
-            self.distance_str.set('{} m'.format(round(self.distance, 2)))
-    
     def connect(self, address):
         """Connect to unit and create object."""
         try:
